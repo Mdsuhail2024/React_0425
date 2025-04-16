d2179635d0463e5e70b340f389ac8f4181fc4492
Mdsuhail2024 <mdsuhail30111@gmail.com>
2025-04-16 15:36:46 +0530

    first commit

---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div>Chai or React || Md Suhail</div>
+      <p>
+        Create React App has been deprecated. that's why every developer use
+        vite and Parsle tool
+      </p>
+    </>
+  );
+};
+
+export default App;
