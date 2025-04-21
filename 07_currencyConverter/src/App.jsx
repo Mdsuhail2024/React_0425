6d10d044c81462c06ab539e9d1c485aa1d6ec036
Mdsuhail2024 <mdsuhail30111@gmail.com>
2025-04-21 12:39:01 +0530

    A big project

--- 07_currencyConverter/src/App.jsx
+++ 07_currencyConverter/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold  text-center mt-12">Currency Converter</h1>
+      <h1 className="text-4xl font-bold  text-center mt-12">A big project currency converter</h1>
     </div>
   );
 };
