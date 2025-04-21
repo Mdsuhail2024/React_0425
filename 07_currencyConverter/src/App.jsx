ec2d10c0c4a7ac8b3ad0aa0b71669962a7acf7fd
Mdsuhail2024 <mdsuhail30111@gmail.com>
2025-04-21 12:25:26 +0530

    A big project currency conveter

---
+++ 07_currencyConverter/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold  text-center mt-12">Currency Converter</h1>
+    </div>
+  );
+};
+
+export default App;
